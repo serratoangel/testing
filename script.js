d9d8ac55ac35400c31f4a5c744413625498c36e7
Angel Serrato <serratoariasangel@gmail.com>
2022-07-18 18:20:29 -0500

    Fix style issue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const panels = document.querySelectorAll('.container-panel')
+const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
